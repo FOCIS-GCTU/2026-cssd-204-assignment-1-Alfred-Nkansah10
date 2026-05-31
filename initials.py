abb44eb014230d2c0abb1c2f7f217b874a440381
Alfred-Nkansah10 <2425402770@live.gctu.edu.gh>
2026-05-31 22:25:33 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,33 +1,33 @@
-
-# File: AAKN.py
-# Description: Prints small initials AAKN and stylized large block letters AAKN.
+# File: initial.py
+# Description: Prints small initials AAKN and stylized large block letters AAK.
 # Assignment Number: 2
 #
-# Name: Awuku Alfred Kwame Nkansah
+# Name: Awuku Alfred Kwame
 # STUDENT ID:  2425402770
-# Email: 2425402770@live.gctu.edu.gh
+# Email: 2425402770@klive.gctu.edu.gh
 # Grader: Augustus Buckman
 #
-# On my honor, Awuku Alfred Kwame Nkansah, this programming assignment is my own work
+# On my honor, Awuku Alfred Kwame , this programming assignment is my own work
 # and I have not provided this code to any other student.
 
 
 def main():
     """Prints a blank line, small initials, and a 10x60 grid of stylized large initials."""
     print()
-    print("...AAKN")
+    print("...AAK")
     print()
 
-    print("........AA..................AA.............KKK......KKK........NNN......NNN........")
-    print(".......A..A................A..A............KKK....KKK..........NNNN.....NNN........")
-    print("......AA..AA..............AA..AA...........KKK..KKK............NNNNN....NNN........")
-    print(".....AA....AA............AA....AA..........KKKKKK..............NNN.NN...NNN........")
-    print("...AAAAAAAAAAAA........AAAAAAAAAAAA........KKKKKK..............NNN..NN..NNN........")
-    print("...AAAAAAAAAAAA........AAAAAAAAAAAA........KKK..KKK............NNN...NN.NNN........")
-    print("...AAAA....AAAA........AAAA....AAAA........KKK....KKK..........NNN....NNNNN........")
-    print("...AAAA....AAAA........AAAA....AAAA........KKK......KKK........NNN.....NNNN........")
-    print("...AAAA....AAAA...**...AAAA....AAAA...**...KKK......KKK...**...NNN......NNN...**...")
-    print("...AAAA....AAAA...**...AAAA....AAAA...**...KKK......KKK...**...NNN......NNN...**...")
+    print("........AA..................AA.............KKK......KKK.....")
+    print(".......A..A................A..A............KKK......KKK.....")
+    print("......AA..AA..............AA..AA...........KKK......KKK.....")
+    print(".....AA....AA............AA....AA..........KKK....KKK.......")
+    print("...AAAAAAAAAAAA........AAAAAAAAAAAA........KKKKKK...........")
+    print("...AAAAAAAAAAAA........AAAAAAAAAAAA........KKK..KKK.........")
+    print("...AAAA....AAAA........AAAA....AAAA........KKK....KKK.......")
+    print("...AAAA....AAAA........AAAA....AAAA........KKK......KKK.....")
+    print("...AAAA....AAAA...**...AAAA....AAAA...**...KKK......KKK...**")
+    print("...AAAA....AAAA...**...AAAA....AAAA...**...KKK......KKK...**")
 
     print()
+
 main()
