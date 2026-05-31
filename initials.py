c25970d95cf6b7159be18257235851ca97793827
Alfred-Nkansah10 <2425402770@live.gctu.edu.gh>
2026-05-31 12:11:07 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
 
+# File: AAKN.py
+# Description: Prints small initials AAKN and stylized large block letters AAKN.
+# Assignment Number: 2
+#
+# Name: Awuku Alfred Kwame Nkansah
+# STUDENT ID:  2425402770
+# Email: 2425402770@klive.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Awuku Alfred Kwame Nkansah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 def main():
-  pass #code goes here
+    """Prints a blank line, small initials, and a 10x60 grid of stylized large initials."""
+    print()
+    print("...AAKN")
+    print()
 
+    print("........AA..................AA.............KKK......KKK........NNN......NNN........")
+    print(".......A..A................A..A............KKK....KKK..........NNNN.....NNN........")
+    print("......AA..AA..............AA..AA...........KKK..KKK............NNNNN....NNN........")
+    print(".....AA....AA............AA....AA..........KKKKKK..............NNN.NN...NNN........")
+    print("...AAAAAAAAAAAA........AAAAAAAAAAAA........KKKKKK..............NNN..NN..NNN........")
+    print("...AAAAAAAAAAAA........AAAAAAAAAAAA........KKK..KKK............NNN...NN.NNN........")
+    print("...AAAA....AAAA........AAAA....AAAA........KKK....KKK..........NNN....NNNNN........")
+    print("...AAAA....AAAA........AAAA....AAAA........KKK......KKK........NNN.....NNNN........")
+    print("...AAAA....AAAA...**...AAAA....AAAA...**...KKK......KKK...**...NNN......NNN...**...")
+    print("...AAAA....AAAA...**...AAAA....AAAA...**...KKK......KKK...**...NNN......NNN...**...")
 
-
+    print()
 main()
